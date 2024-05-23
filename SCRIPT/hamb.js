369d3471602445d069a0161bf2e83e4cb9b080c8
RosaHReddi <114524777+RosaHReddi@users.noreply.github.com>
2024-05-23 18:49:48 -0300

    Add files via upload

--- SCRIPT/hamb.js
+++ SCRIPT/hamb.js
@@ -15,3 +15,112 @@ function cerrar() {
   boton_abrir.classList.remove("abierto");
   nav.classList.remove("abierto");
 }
+
+
+/*-------------VALIDAR FORMULARIOS-------------*/
+
+function validar1(event) {
+
+  event.preventDefault();
+
+  let nombre1 = document.getElementById("nombre").value.trim();
+  let fecha = document.getElementById("fecha").value;
+  let horario = document.getElementById("horario").value;
+  let personas = document.getElementById("personas").value;
+  let respuesta = document.getElementById("respuesta1");
+
+  if (nombre1 == "" || horario == "" || personas == "" || fecha == "") {
+    respuesta.innerHTML = "Complete todos los campos marcados con *"
+    setTimeout(() => {
+      respuesta.innerHTML = "";
+    }, 1500);
+    return false
+  }
+
+  respuesta.innerHTML = "¡Reserva enviada!"
+  document.getElementById("formulario1").reset();
+  setTimeout(() => {
+    respuesta.innerHTML = "";
+  }, 2500);
+  return true;
+}
+
+function validar2(event) {
+
+  event.preventDefault();
+
+  let nombre2 = document.getElementById("nombre2").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let mensaje = document.getElementById("mensaje").value.trim();
+  let respuesta2 = document.getElementById("respuesta2");
+
+  if (nombre2 == "" || email == "" || mensaje == "") {
+    respuesta2.innerHTML = "Complete todos los campos marcados con *"
+    setTimeout(() => {
+      respuesta2.innerHTML = "";
+    }, 1500);
+    return false
+  }
+
+  respuesta2.innerHTML = "¡Mensaje enviado!"
+  document.getElementById("formulario2").reset();
+  setTimeout(() => {
+    respuesta2.innerHTML = "";
+  }, 2500);
+  return true;
+}
+
+document.getElementById("formulario1").addEventListener("submit", function (event) {
+  validar1(event);
+});
+
+document.getElementById("formulario2").addEventListener("submit", function (event) {
+  validar2(event);
+});
+
+
+
+/*-------------RESALTADO DE SECCION-------------*/
+
+let todos = document.getElementsByClassName("res");
+console.log(todos);
+for (let i = 0; i < todos.length; i++) {
+  todos[i].addEventListener("click", function (e) {
+
+    e.preventDefault();
+
+    let targetId = this.getAttribute('href');
+
+    let targetSection;
+    switch (targetId) {
+      case '#reservas':
+        targetSection = document.getElementById("seccion-tres");
+        break;
+      case "#ubicacion":
+        targetSection = document.getElementById("seccion-cuatro");
+        break;
+      case "#contacto":
+        targetSection = document.getElementById("seccion-ocho");
+        break;
+    };
+
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+
+    targetSection.style.backgroundColor = 'rgb(255, 239, 215)';
+    targetSection.style.border = '2px dotted green';
+    setTimeout(() => {
+      targetSection.style.backgroundColor = '';
+      targetSection.style.border = '2px solid rgb(194, 6, 6)';
+    }, 500);
+    
+    setTimeout(() => {
+      targetSection.style.backgroundColor = 'rgb(255, 239, 215)';
+      targetSection.style.border = '2px dashed blue';
+    }, 1000);
+
+    setTimeout(() => {
+      targetSection.style.backgroundColor = '';
+      targetSection.style.border = '2px solid rgb(194, 6, 6)';
+    }, 1500);
+  })
+}
